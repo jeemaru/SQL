6386e3ef096d53f1678ce8ebf3c270925956bd24
jeemaru <4iiii@hanmail.net>
2022-05-11 18:42:32 +0900

    2022-05-11 18:42

--- Ex/Ex01.sql
+++ Ex/Ex01.sql
@@ -1,7 +1,7 @@
 /*select 문 기본*/
 
-select * from
-employees;
+select * from employees;
 
-select *from 
-departments;
+select *from departments;
+
+select employee_id first_name, phone_number from employees;
